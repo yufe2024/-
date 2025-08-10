31336a5ded2a8d4e006ce5125fc6d3b3fa9c782a
yufe2024 <2528956202@qq.com>
2025-08-10 21:52:05 +0800

    Add files via upload

--- js/server.js
+++ js/server.js
@@ -1,15 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const PORT = 3000;//这个是端口
-app.use(cors(
-    {
-        origin:'https://yufe2024.github.io/-/'
-    }
-)
-
-)
-app.listen(PORT, () => {
-  console.log(`后端服务运行在 http://localhost:${PORT}`);
-  console.log(`测试接口：http://localhost:${PORT}/api/messages`);
-});
+require("http").createServer((request,response)=>{
+  response.end("Hello World");
+}).listen(3000);
